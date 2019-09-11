9c65e8501f36e21acd803124764425bc1a3f50aa
gyogo <s2018w07@e-mirim.js.kr>
2019-09-11 10:06:52 +0900

    ComboBox JRadioButton TextField ButtonGroup

---
+++ 09.06_GUI1/src/GUI_Components.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GUI_Components {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Components");
+        JPanel panel = new JPanel();
+        JLabel lb = new JLabel("JLabel");
+        String [] con= {"glgl","meow"};
+        JButton button = new JButton("Jbutton");
+        JCheckBox ck1 = new JCheckBox("강아지");
+        JComboBox ck0 = new JComboBox(con);
+        JCheckBox ck2 = new JCheckBox("고양이");
+        JRadioButton rb1 = new JRadioButton("미성년자");
+        JRadioButton rb2 = new JRadioButton("성인");
+        ButtonGroup bg = new ButtonGroup();
+        bg.add(rb1);
+        bg.add(rb2);
+        JTextField tf0 = new JTextField("JTextField");
+        JTextArea ta0 = new JTextArea(2,10);
+
+        panel.add(lb);
+        panel.add(button);
+        panel.add(ck1);
+        panel.add(ck2);
+        panel.add(ck0);
+        panel.add(rb1);
+        panel.add(rb2);
+        panel.add(tf0);
+        panel.add(ta0);
+
+        frame.add(panel);
+
+        frame.setPreferredSize(new Dimension(600,400));
+        frame.pack();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+    }
+}
